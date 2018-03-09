1226eb4f0f07665c77afa4a52c703294449e5cdb
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-03-09 16:14:49 +0300

    добавила метод setField использовала Split() Спасибо :)

--- src/lesson1/OwnSerialize/Application.java
+++ src/lesson1/OwnSerialize/Application.java
@@ -3,10 +3,12 @@ package lesson1.OwnSerialize;
 import lesson1.Monitor;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("C:/Users/pesto/IdeaProjects/Java2_pestovnikova/src/lesson1/OwnSerialize/monitor_ver2.txt");
-        Monitor[] monitors = OwnReadClass.readFromTXT(file);
+        ArrayList<Monitor> monitors = OwnReadClass.readFromTXT(file);
     }
 }
--- src/lesson1/OwnSerialize/OwnReadClass.java
+++ src/lesson1/OwnSerialize/OwnReadClass.java
@@ -7,54 +7,35 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class OwnReadClass {
-    static Monitor[] monitors;
 
-    public static Monitor[] readFromTXT(File file){
+    public static ArrayList<Monitor> readFromTXT(File file) throws IOException {
+        ArrayList<Monitor> monitors = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-            ArrayList<String> strings = new ArrayList<String>();
             while ((line = reader.readLine()) != null) {
-                StringBuilder stringBuilder = new StringBuilder();
-                char[] arrayChar = line.toCharArray();
-                int i = 0;
-                while (i < arrayChar.length) {
-                    if (arrayChar[i] != ',') {
-                        stringBuilder.append(arrayChar[i]);
-                    } else {
-                        strings.add(stringBuilder.toString());
-                        stringBuilder.setLength(0);
-                    }
-                    i++;
-                }
-                strings.add(stringBuilder.toString());
-            }
-            int countMonitor = strings.size() / 3;
-            int j = 0;
-            monitors = new Monitor[countMonitor];
-            for (int i = 0; i < strings.size(); i = i + 3) {
-                String name = strings.get(i);
-                String model = strings.get(i + 1);
-                int price = Integer.parseInt(strings.get(i + 2));
-                monitors[j] = new Monitor();
-                Class monitorClass = monitors[j].getClass();
+                String[] array = line.split(",");
+                Monitor currentMonitor = new Monitor();
                 try {
+                    Class monitorClass = currentMonitor.getClass();
                     Field nameMonitor = monitorClass.getDeclaredField("name");
+                    setField(nameMonitor, currentMonitor, array[0]);
+
                     Field modelMonitor = monitorClass.getDeclaredField("model");
+                    setField(modelMonitor, currentMonitor, array[1]);
+
                     Field priceMonitor = monitorClass.getDeclaredField("price");
-                    nameMonitor.setAccessible(true);
-                    nameMonitor.set(monitors[j], name);
-                    modelMonitor.setAccessible(true);
-                    modelMonitor.set(monitors[j], model);
-                    priceMonitor.setAccessible(true);
-                    priceMonitor.set(monitors[j], price);
+                    setField(priceMonitor, currentMonitor, Integer.parseInt(array[2]));
                 } catch (ReflectiveOperationException e) {
                     e.printStackTrace();
                 }
-                j++;
+                monitors.add(currentMonitor);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return monitors;
+    }
+
+    public static void setField(Field field, Object object, Object data) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(object, data);
     }
 }
