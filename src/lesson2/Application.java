83d3265e399d88591a854af0c497f3762c0f5bf4
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-03-12 16:58:36 +0300

    получение всех классов в пакете

--- src/lesson2/Application.java
+++ src/lesson2/Application.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         String namePackage = "lesson2";
 
@@ -15,17 +15,21 @@ public class Application {
 
     }
 
-    private static List<Class> getClassesInDirectory(File dir, String namePackage) {
+    private static List<Class> getClassesInDirectory(File dir, String namePackage) throws ClassNotFoundException {
         List<Class> listClass = new ArrayList<>();
         File[] files = dir.listFiles();
 
         if (files == null) return listClass;
 
         for (File file : files) {
-
+            if (file.isDirectory()) {
+                listClass.addAll(getClassesInDirectory(file, namePackage + "." + file.getName()));
+            } else if (file.getName().endsWith(".java")) {
+                listClass.add(Class.forName(namePackage + "." + file.getName().replace(".java", "")));
+            }
         }
 
-        return null;
+        return listClass;
     }
 
 }
