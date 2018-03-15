fe045b584f88773d4c8665f90e880647a797ca15
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-03-16 00:07:46 +0300

    Write test on lesson 3 (jupiter)

--- src/test/java/lesson3/StringUtilTest.java
+++ src/test/java/lesson3/StringUtilTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test;
 /**
  * Класс можно назвать TestStringUtil
  */
+
+// иногда пишут public private class - чтобы не переопределять
 public class StringUtilTest {
 
+    // private - не надо создавать объекты этого класса, т.к. тут и так все методы public
+    private StringUtilTest() {
+
+    };
     // правило написания имени метода теста: testIsEmpty_nullString_returnTrue
     // если передадим nullString, то получим True
 
     // @Test для того, чтобы библиотека jupiter поняла, что это тестовы метод
     @Test
     public void testIsEmpty_nullString_returnTrue() {
-        boolean result = lesson3.StringUtil.isEmpty(null);
+        boolean result = StringUtil.isEmpty(null);
         // assert - ждать
         Assertions.assertTrue(result); // если передаем True, то тест не упадет
     }
