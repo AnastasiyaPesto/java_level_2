a9ce1afcc75b8ecabd285ed606c2096cb40a08b4
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-03-30 11:07:04 +0300

    [*] add setup() in test

---
+++ src/test/java/lesson6/dao/MySqlDepartmentDaoImplTest.java
@@ -0,0 +1,37 @@
+package lesson6.dao;
+
+import lesson6.configuration.Configuration;
+import lesson6.configuration.PropertiesJdbcConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlDepartmentDaoImplTest {
+
+    private MySqlDepartmentDaoImpl mySqlDepartmentDao;
+
+    @BeforeEach
+    public void setup() throws IOException, SQLException {
+        Configuration config = new PropertiesJdbcConfiguration().load();
+        String url = "jdbc:mysql://" + config.getJdbcHost() + ":" +
+                config.getJdbcPort() + "/" + config.getJdbcDatabaseName();
+        if (config.getJdbcTimeZone() != null) {
+            url = url + "?serverTimezone=" + config.getJdbcTimeZone();
+        }
+        String user = config.getJdbcUserName();
+        String pasw = config.getJdbcPassword();
+        try (Connection connection = DriverManager.getConnection(url, user, pasw)) {
+            this.mySqlDepartmentDao = new MySqlDepartmentDaoImpl(connection);
+        }
+    }
+
+    @Test
+    public void testCreate_objectNotNull() {
+
+    }
+
+}
