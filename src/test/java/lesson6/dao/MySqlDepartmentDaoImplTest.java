e3be9cb61616c15e3d1fc766e4fa036799c3128e
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-03-30 11:50:11 +0300

    [*] add static block for download Driver in test

--- src/test/java/lesson6/dao/MySqlDepartmentDaoImplTest.java
+++ src/test/java/lesson6/dao/MySqlDepartmentDaoImplTest.java
@@ -10,7 +10,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+
 public class MySqlDepartmentDaoImplTest {
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private MySqlDepartmentDaoImpl mySqlDepartmentDao;
 
